f99c1be179e4c104d4d26a6054dc57fe0c201cf8
moqbelAbd <yabd390@gmail.com>
2026-09-01 09:58:34 +0300

    Initialized  backend project and configured it & Added Dependencies

---
+++ FindExpertsBackend/FindExpertsBackend/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FindExpertsBackend.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FindExpertsBackend.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
