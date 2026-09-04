39d4558868c5288451eb38745a4d3da65bce1857
moqbelAbd <yabd390@gmail.com>
2026-09-04 07:26:38 +0300

    Deleteing merged files

--- FindExpertsBackend/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace FindExpertsBackend.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-        public string? FullName { get; set; }
-    }
-}
